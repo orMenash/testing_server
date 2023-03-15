f7cfc270c32d412dc33d09e59d5521664dcca58d
Or Menashe <ormenash@gmail.com>
2023-03-15 22:12:00 +0200

    delete cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.listen(port, function() {
   console.log(`Server listening on port ${port}`);
 });
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
